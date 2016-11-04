4d19f6ee303e0dff8edd37bf08a70d6052259251
최성근 <ccc3132@naver.com>
2016-11-04 12:32:53 +0900

    1번메뉴 추가

---
+++ 2011111/src/SecondMax.java
@@ -0,0 +1,51 @@
+import java.util.*;
+ public class SecondMax extends ExamVariousArray{
+		int o,input[],second;
+	void Sestart()
+	{
+		System.out.println("2.1번메뉴 클래스 :SecondMax (배열사용)");
+		System.out.println("10개의 숫자를 입력받아 배열에 저장하고 \n");
+		System.out.println("두번째 큰 수를 출력하라..");
+		System.out.println("=============================== ");				
+		
+	 Culluc();
+	}
+	
+	void Culluc()
+	{
+		Scanner s = new Scanner(System.in);
+		this.o = s.nextInt();  
+		if(input[0] > input[1])
+		   {
+              o = input[0];
+              second = input[1];
+          }
+          else 
+          {
+              o = input[1];
+              second = input[0];
+          }
+
+          for(int i = 2; i < 10; i++) 
+          {
+              if((input[i] <= o) && input[i] > second)
+              {
+                  second = input[i];
+              }
+
+              if(input[i] > o) 
+              {
+                  second = o;
+                  o = input[i];
+              }
+          }
+
+          
+          print();
+	}
+	void print(){
+		System.out.println("두번째로 큰 수는"+second+"입니다.");
+	}
+	
+
+}
